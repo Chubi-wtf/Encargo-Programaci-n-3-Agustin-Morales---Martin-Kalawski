0fb459a8decaf9211422dcedc2f95503c0ede1ab
Chubi <agustin.morales.olate42@gmail.com>
2025-11-02 03:59:48 -0300

    Cosas hechas en brote psicotico

--- Assets/Scripts/ItemInstance.cs
+++ Assets/Scripts/ItemInstance.cs
@@ -1,6 +1,41 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class ItemInstance : MonoBehaviour
 {
-    public ItemData data;
+    #region Variables
+    public ItemData datosDelItem;
+    private Rigidbody miRigidbody;
+    private const string tagCestaCarrito = "CartBasket";
+    #endregion
+
+    #region Eventos de Unity
+    void Awake()
+    {
+        miRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider otroCollider)
+    {
+        if (otroCollider.CompareTag(tagCestaCarrito))
+        {
+            miRigidbody.isKinematic = true;
+
+            Rigidbody rbDelCarrito = otroCollider.GetComponentInParent<Rigidbody>();
+            if (rbDelCarrito != null)
+            {
+                transform.SetParent(rbDelCarrito.transform);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider otroCollider)
+    {
+        if (otroCollider.CompareTag(tagCestaCarrito))
+        {
+            miRigidbody.isKinematic = false;
+            transform.SetParent(null);
+        }
+    }
+    #endregion
 }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,174 +2,287 @@ using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 public class PlayerController : MonoBehaviour
 {
+    #region Variables del Inspector
     [Header("Componentes")]
-    [SerializeField] private Transform playerCamera;
-    private Rigidbody rb;
+    [SerializeField] private Transform camaraDelPlayer;
 
     [Header("Movimiento")]
-    [SerializeField] private float walkSpeed = 5f;
-    [SerializeField] private float runSpeed = 8f;
-    private float currentSpeed;
+    [SerializeField] private float velocidadCaminar = 5f;
+    [SerializeField] private float velocidadCorrer = 8f;
 
     [Header("Cámara")]
-    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private float sensibilidadMouse = 100f;
 
     [Header("Interacción (Raycast)")]
-    [SerializeField] private float raycastDistance = 3f;
-    [SerializeField] private LayerMask interactableLayer;
+    [SerializeField] private float distanciaRaycast = 3f;
+    [SerializeField] private LayerMask capaInteractuable;
+    [SerializeField] private LayerMask capaPiso;
 
     [Header("Interacción (Agarrar)")]
-    [SerializeField] private Transform pickupHolder; 
-    [SerializeField] private KeyCode interactionKey = KeyCode.E; 
+    [SerializeField] private Transform holderParaItems;
+    [SerializeField] private KeyCode teclaInteractuar = KeyCode.E;
 
-    private GameObject heldObject; 
-    private Rigidbody heldObjectRb; 
+    [Header("Interacción (Carrito)")]
+    [SerializeField] private string tagDelCarrito = "Carrito";
+    [SerializeField] private float suavidadSeguirCarrito = 15f;
+    [SerializeField] private float alturaFlotarCarrito = 0.2f;
 
     [Header("UI de Interacción")]
-    [SerializeField] private GameObject itemInfoPanel;
-    [SerializeField] private TextMeshProUGUI itemNameText;
-    [SerializeField] private TextMeshProUGUI itemPriceText;
+    [SerializeField] private GameObject panelInfoItem;
+    [SerializeField] private TextMeshProUGUI textoNombreItem;
+    [SerializeField] private TextMeshProUGUI textoPrecioItem;
+    #endregion
 
-    private float xRotation = 0f;
-    private float yRotation = 0f;
-    private Vector3 moveInput;
+    #region Variables Privadas
+    private Rigidbody miRigidbody;
+    private Collider miCollider;
+    private float velocidadActual;
 
+    private GameObject objetoAgarrado;
+    private Rigidbody rbObjetoAgarrado;
+    private Collider colliderObjetoAgarrado;
+
+    private Rigidbody rbCarritoAgarrado = null;
+    private Collider colliderCarritoAgarrado = null;
+    private Vector3 offsetPosCarrito;
+    private Quaternion offsetRotCarrito;
+
+    private float rotacionX = 0f;
+    private float rotacionY = 0f;
+    private Vector3 inputMovimiento;
+    #endregion
+
+    #region Eventos de Unity
     void Awake()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        miRigidbody = GetComponent<Rigidbody>();
+        miCollider = GetComponent<Collider>();
+        miRigidbody.freezeRotation = true;
     }
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        currentSpeed = walkSpeed;
+        velocidadActual = velocidadCaminar;
 
-        if (itemInfoPanel != null)
+        if (panelInfoItem != null)
         {
-            itemInfoPanel.SetActive(false);
+            panelInfoItem.SetActive(false);
         }
     }
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseInputX = Input.GetAxis("Mouse X") * sensibilidadMouse * Time.deltaTime;
+        float mouseInputY = Input.GetAxis("Mouse Y") * sensibilidadMouse * Time.deltaTime;
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        rotacionY += mouseInputX;
+        rotacionX -= mouseInputY;
+        rotacionX = Mathf.Clamp(rotacionX, -90f, 90f);
 
-        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
-        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.rotation = Quaternion.Euler(0f, rotacionY, 0f);
+        camaraDelPlayer.localRotation = Quaternion.Euler(rotacionX, 0f, 0f);
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        moveInput = (transform.forward * verticalInput + transform.right * horizontalInput).normalized;
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        inputMovimiento = (transform.forward * inputV + transform.right * inputH).normalized;
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            currentSpeed = runSpeed;
+            velocidadActual = velocidadCorrer;
         }
         else
         {
-            currentSpeed = walkSpeed;
+            velocidadActual = velocidadCaminar;
         }
 
-        HandleRaycastUI(); 
-        HandleInteractionInput(); 
-    }
-
-    private void HandleRaycastUI()
-    {
-        if (heldObject != null)
-        {
-            itemInfoPanel.SetActive(false);
-            return;
-        }
-
-        Ray ray = new Ray(playerCamera.position, playerCamera.forward);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, raycastDistance, interactableLayer))
-        {
-            ItemInstance item = hit.collider.GetComponent<ItemInstance>();
-            if (item != null)
-            {
-                itemInfoPanel.SetActive(true);
-                itemNameText.text = item.data.itemName;
-                itemPriceText.text = $"${item.data.price} - {item.data.itemCategory}";
-            }
-            else
-            {
-                itemInfoPanel.SetActive(false);
-            }
-        }
-        else
-        {
-            itemInfoPanel.SetActive(false);
-        }
-    }
-
-    private void HandleInteractionInput()
-    {
-        if (Input.GetKeyDown(interactionKey))
-        {
-            if (heldObject != null)
-            {
-                DropObject();
-            }
-            else
-            {
-                Ray ray = new Ray(playerCamera.position, playerCamera.forward);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit, raycastDistance, interactableLayer))
-                {
-                    ItemInstance item = hit.collider.GetComponent<ItemInstance>();
-                    if (item != null)
-                    {
-                        PickUpObject(item.gameObject);
-                    }
-                }
-            }
-        }
-    }
-
-    private void PickUpObject(GameObject item)
-    {
-        heldObject = item;
-        heldObjectRb = item.GetComponent<Rigidbody>();
-
-        
-        heldObjectRb.isKinematic = true;
-
-        heldObject.transform.SetParent(pickupHolder);
-
-        heldObject.transform.localPosition = Vector3.zero;
-        heldObject.transform.localRotation = Quaternion.identity; 
-
-        heldObject.layer = LayerMask.NameToLayer("Ignore Raycast");
-    }
-
-    private void DropObject()
-    {
-        heldObjectRb.isKinematic = false;
-
-        heldObject.transform.SetParent(null);
-
-        heldObject.layer = LayerMask.NameToLayer("Interactable");
-
-        heldObject = null;
-        heldObjectRb = null;
+        ManejarRaycastUI();
+        ManejarInputInteraccion();
     }
 
     void FixedUpdate()
     {
-        Vector3 targetVelocity = moveInput * currentSpeed;
-        targetVelocity.y = rb.linearVelocity.y; 
-        rb.linearVelocity = targetVelocity;
+        Vector3 velocidadObjetivo = inputMovimiento * velocidadActual;
+        velocidadObjetivo.y = miRigidbody.linearVelocity.y;
+        miRigidbody.linearVelocity = velocidadObjetivo;
+
+        if (rbCarritoAgarrado != null)
+        {
+            ManejarFisicasCarrito();
+        }
     }
+    #endregion
+
+    #region Lógica de Interacción y UI
+    private void ManejarRaycastUI()
+    {
+        if (objetoAgarrado != null || rbCarritoAgarrado != null)
+        {
+            panelInfoItem.SetActive(false);
+            return;
+        }
+
+        Ray rayo = new Ray(camaraDelPlayer.position, camaraDelPlayer.forward);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(rayo, out hitInfo, distanciaRaycast, capaInteractuable))
+        {
+            ItemInstance itemInstance = hitInfo.collider.GetComponent<ItemInstance>();
+            if (itemInstance != null)
+            {
+                panelInfoItem.SetActive(true);
+                textoNombreItem.text = itemInstance.datosDelItem.itemName;
+                textoPrecioItem.text = $"${itemInstance.datosDelItem.price} - {itemInstance.datosDelItem.itemCategory}";
+            }
+            else
+            {
+                panelInfoItem.SetActive(false);
+            }
+        }
+        else
+        {
+            panelInfoItem.SetActive(false);
+        }
+    }
+
+    private void ManejarInputInteraccion()
+    {
+        if (Input.GetKeyDown(teclaInteractuar))
+        {
+            if (rbCarritoAgarrado != null)
+            {
+                SoltarCarrito();
+                return;
+            }
+
+            if (objetoAgarrado != null)
+            {
+                SoltarObjeto();
+                return;
+            }
+
+            Ray rayo = new Ray(camaraDelPlayer.position, camaraDelPlayer.forward);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayo, out hitInfo, distanciaRaycast, capaInteractuable))
+            {
+                ItemInstance itemInstance = hitInfo.collider.GetComponent<ItemInstance>();
+                if (itemInstance != null)
+                {
+                    AgarrarObjeto(itemInstance.gameObject);
+                }
+                else if (hitInfo.collider.CompareTag(tagDelCarrito))
+                {
+                    AgarrarCarrito(hitInfo.collider.gameObject);
+                }
+                else if (hitInfo.collider.transform.parent != null && hitInfo.collider.transform.parent.CompareTag(tagDelCarrito))
+                {
+                    AgarrarCarrito(hitInfo.collider.transform.parent.gameObject);
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region Lógica del Carrito
+    private void AgarrarCarrito(GameObject objetoCarrito)
+    {
+        Rigidbody rbDelCarrito = objetoCarrito.GetComponentInParent<Rigidbody>();
+        if (rbDelCarrito == null) return;
+
+        rbCarritoAgarrado = rbDelCarrito;
+
+        offsetPosCarrito = transform.InverseTransformPoint(rbCarritoAgarrado.position);
+        offsetRotCarrito = Quaternion.Inverse(transform.rotation) * rbCarritoAgarrado.rotation;
+
+        colliderCarritoAgarrado = objetoCarrito.GetComponentInParent<Collider>();
+        if (colliderCarritoAgarrado != null && miCollider != null)
+        {
+            Physics.IgnoreCollision(miCollider, colliderCarritoAgarrado, true);
+        }
+
+        rbCarritoAgarrado.useGravity = false;
+    }
+
+    private void SoltarCarrito()
+    {
+        if (colliderCarritoAgarrado != null && miCollider != null)
+        {
+            Physics.IgnoreCollision(miCollider, colliderCarritoAgarrado, false);
+        }
+
+        if (rbCarritoAgarrado != null)
+        {
+            rbCarritoAgarrado.useGravity = true;
+        }
+
+        rbCarritoAgarrado = null;
+        colliderCarritoAgarrado = null;
+    }
+
+    private void ManejarFisicasCarrito()
+    {
+        Quaternion rotacionObjetivo = transform.rotation * offsetRotCarrito;
+
+        Quaternion rotacionSuave = Quaternion.Slerp(
+            rbCarritoAgarrado.rotation,
+            rotacionObjetivo,
+            suavidadSeguirCarrito * Time.fixedDeltaTime
+        );
+        rbCarritoAgarrado.MoveRotation(rotacionSuave);
+
+        Vector3 posicionObjetivo = transform.TransformPoint(offsetPosCarrito);
+
+        RaycastHit hitInfo;
+        Vector3 puntoChequeoPiso = new Vector3(posicionObjetivo.x, transform.position.y + 1f, posicionObjetivo.z);
+
+        if (Physics.Raycast(puntoChequeoPiso, Vector3.down, out hitInfo, 2f, capaPiso))
+        {
+            posicionObjetivo.y = hitInfo.point.y + alturaFlotarCarrito;
+        }
+
+        Vector3 nuevaPosicion = Vector3.Lerp(rbCarritoAgarrado.position, posicionObjetivo, suavidadSeguirCarrito * Time.fixedDeltaTime);
+        rbCarritoAgarrado.MovePosition(nuevaPosicion);
+    }
+    #endregion
+
+    #region Lógica de Items
+    private void AgarrarObjeto(GameObject itemParaAgarrar)
+    {
+        objetoAgarrado = itemParaAgarrar;
+        rbObjetoAgarrado = itemParaAgarrar.GetComponent<Rigidbody>();
+        colliderObjetoAgarrado = itemParaAgarrar.GetComponent<Collider>();
+
+        rbObjetoAgarrado.isKinematic = true;
+        objetoAgarrado.transform.SetParent(holderParaItems);
+        objetoAgarrado.transform.localPosition = Vector3.zero;
+        objetoAgarrado.transform.localRotation = Quaternion.identity;
+
+        if (miCollider != null && colliderObjetoAgarrado != null)
+        {
+            Physics.IgnoreCollision(miCollider, colliderObjetoAgarrado, true);
+        }
+
+        objetoAgarrado.layer = LayerMask.NameToLayer("Ignore Raycast");
+    }
+    private void SoltarObjeto()
+    {
+        if (miCollider != null && colliderObjetoAgarrado != null)
+        {
+            Physics.IgnoreCollision(miCollider, colliderObjetoAgarrado, false);
+        }
+
+        rbObjetoAgarrado.isKinematic = false;
+        objetoAgarrado.transform.SetParent(null);
+        objetoAgarrado.layer = LayerMask.NameToLayer("Interactable");
+
+        objetoAgarrado = null;
+        rbObjetoAgarrado = null;
+        colliderObjetoAgarrado = null;
+    }
+    #endregion
 }
